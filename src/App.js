9fa2dd398b1fb8bf5916b4c4f7f83bcbd2c32b27
DanielCortild <daniel@cortild.dk>
2020-04-27 10:30:44 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {Cards, Chart, CountryPicker} from './components';
+import styles from './App.module.css';
+import {fetchData} from './api';
+
+import coronaImg from './images/image.png';
+
+class App extends React.Component {
+
+  state = {
+    data: {},
+    country: ''
+  }
+
+  async componentDidMount() {
+    this.handleCountry();
+  }
+
+  handleCountry = async (country) => {
+    const fetchedData = await fetchData(country);
+    this.setState({ data: fetchedData, country: country });
+  }
+
+  render() {
+    const {data, country} = this.state
+
+    return(
+      <React.Fragment>
+        <div className={styles.imgDiv}>
+          <img className={styles.image} src={coronaImg} alt="COVID-19"/>
+        </div>
+        <div className={styles.container}>
+          <div className={styles.cards}><Cards data={data}/></div>
+          <div className={styles.subContainer}>
+            <CountryPicker handleCountry={this.handleCountry}/>
+            <Chart data={data} country={country}/>
+          </div>
+        </div>
+      </React.Fragment>
+    )
+  }
 }
 
 export default App;
