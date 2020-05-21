3a84277f0291937f9924db875d4282117d1c5137
Daniel Cortild <daniel.cortild@gmail.com>
2020-05-21 17:27:50 +0200

    Updates

---
+++ precache-manifest.05f56d6ee9874b6ff8087eca6719dd49.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74862ffa1be3054d96b89283e4a63c08",
+    "url": "/Covid19Tracker/index.html"
+  },
+  {
+    "revision": "4a6b23855e9a3dee8e3d",
+    "url": "/Covid19Tracker/static/css/main.d20a9d07.chunk.css"
+  },
+  {
+    "revision": "4b87e7cb0fe8d648b3ea",
+    "url": "/Covid19Tracker/static/js/2.8d8f367b.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Covid19Tracker/static/js/2.8d8f367b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a6b23855e9a3dee8e3d",
+    "url": "/Covid19Tracker/static/js/main.1774a4c7.chunk.js"
+  },
+  {
+    "revision": "f512d62b250d1df36766",
+    "url": "/Covid19Tracker/static/js/runtime-main.201707b8.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/Covid19Tracker/static/media/image.d7265326.png"
+  }
+]);
